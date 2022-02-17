3929462f30e8c8ff5bee82301a1f6e177bf4302d
Cesar Vargas <pseudohumanoide@gmail.com>
2022-02-17 16:49:06 -0600

    first commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent{
+
+  heroes: string[] = ['Spiderman','Ironan','Hulk','Thor'];
+  heroeBorrado: string  = '';
+
+  borrarHeroe(): void{
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
+
+}
